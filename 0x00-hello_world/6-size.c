ac6036ccf57392eef25f06e097d03c4b83467992
Nwankwo Uchenna <nwankwouche52@gmail.com>
2022-09-12 19:25:24 -0700

    Size fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	char c;
+	int i;
+	long li;
+	long long lii;
 	float f;
 
-	printf("size of an int: %ld byte(s)\n", sizeof(a));
-	printf("size of a long int: %ld byte(s)\n", sizeof(b));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a char: %ld byte(s)\n", sizeof(d));
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of a int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
