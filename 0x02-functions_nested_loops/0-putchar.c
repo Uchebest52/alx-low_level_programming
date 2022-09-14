7dc457ccc6af435875203521cd81562d8b1c3803
Nwankwo Uchenna <nwankwouche52@gmail.com>
2022-09-14 09:27:59 -0700

    putchar.c fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,6 +15,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n);
 
 	return (0);
 
