325a86517cc28ad908956b4207547184bce87e55
Nwankwo Uchenna <nwankwouche52@gmail.com>
2022-09-15 06:00:33 -0700

    sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar('+');
-	return (1)
+	return (1);
 }
 
 	else if (n < 0)
@@ -19,6 +19,7 @@ int print_sign(int n)
 	}
 	else
 	{
-	_putchar('0'); / return (0);
+	_putchar('0');
+	return (0);
 	}
 }
